48cf67e3e8f94a85b0d74fdb7b53d45aeebd147b
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-06-09 00:46:41 +0500

    delete context

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { navbar } from "../utils/navbar";
 
 export default function Root() {
    return (
       <BrowserRouter>
          <Routes>
-            <Route path="/home" element={<h1>home</h1>} />
-            <Route path="/properties" element={<h1>properties</h1>} />
+            {navbar.map(({ id, path, element }) => (
+               <Route key={id} path={path} element={element} />
+            ))}
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path="*" element={<h1>404 Not Fond</h1>} />
          </Routes>
