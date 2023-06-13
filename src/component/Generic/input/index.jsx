be638769d92b2bb2b54c9afff2bc611269bf9cf4
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-06-13 16:14:27 +0500

    redirect

--- src/component/Generic/input/index.jsx
+++ src/component/Generic/input/index.jsx
@@ -2,10 +2,10 @@ import React, { forwardRef } from "react";
 import { InputWrap } from "./style";
 import useUniqueId from "../../../hooks/useId";
 
-export const Input = forwardRef(({ type, placeholder, width, height, icon, bg , id}, ref) => {
+export const Input = forwardRef(({ type, placeholder, width, height, icon, bg}, ref) => {
    const useId = useUniqueId();
    return (
-      <InputWrap ref={ref} key={id} width={width} height={height} bg={bg}>
+      <InputWrap ref={ref} width={width} height={height} bg={bg}>
          {icon && <i className={icon}></i>}
          <InputWrap.input placeholder={placeholder} type={type} />
       </InputWrap>
