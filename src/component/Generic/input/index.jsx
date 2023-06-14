eda690d8299a1dbb3efc6b026f0c0b07aeb57dd8
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-06-15 00:40:49 +0500

    dynamic useRef

--- src/component/Generic/input/index.jsx
+++ src/component/Generic/input/index.jsx
@@ -2,8 +2,9 @@ import React, { forwardRef } from "react";
 import { InputWrap } from "./style";
 
 export const Input = forwardRef(({ onClick, type, placeholder, width, height, icon, bg}, ref) => {
+
    return (
-      <InputWrap onClick={onClick} ref={ref} width={width} height={height} bg={bg}>
+      <InputWrap ref = {ref} onClick={onClick} type = {type} width={width} height={height} bg={bg}>
          {icon && <i className={icon}></i>}
          <InputWrap.Input placeholder={placeholder} type={type} />
       </InputWrap>
--- src/component/Search/filter/index.jsx
+++ src/component/Search/filter/index.jsx
@@ -1,28 +1,33 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Filter } from "./style";
 import { Global } from "../../../root/styled";
 import Input from "../../Generic/input";
 import { FilterData } from "../../../utils/Filter";
 import { Button } from "../../Generic";
 
-export const SearchFilter = ({setOpen}) => {
-   const [refPick, setRefPick] = useState('')
+export const SearchFilter = ({ setDropdownStatus }) => {
+   let [refPick, serRefPick] = useState({})
+   const allRef = useRef({})
+   console.log(refPick);
    return (
       <Filter>
          {FilterData.map(({ id, subtitle, inputs }) => (
             <Filter.Block key={id}>
                <Global.H4 fw={600}>{subtitle}</Global.H4>
-               <Filter.Inputs>
-                  {inputs.map(({ id, type, placeholder, ref}) => (
-                     <Input onClick = {() => setRefPick(ref)} key={id} type={type} placeholder={placeholder} />
+               <Filter.Inputs >
+                  {inputs.map(({ id, type, placeholder, ref: refInput}) => (
+                     <Input key={id} onClick = {() => serRefPick(allRef.current[refInput])} ref = {el => {allRef.current[refInput] = el}} type={type} placeholder={placeholder} />
                   ))}
                </Filter.Inputs>
-            </Filter.Block>
+            </Filter.Block> 
          ))}
-         <Filter.Footer>
-            <Button onClick={() => setOpen(false)} type="cyanBlue">Cancel</Button>
-            <Button  onClick={() => setOpen(false)}>Submit</Button>
+         <Filter.Footer> 
+            <Button onClick={() => setDropdownStatus(false)} type="cyanBlue">
+               Cancel
+            </Button>
+            <Button onClick={() => setDropdownStatus(false)}>Submit</Button>
          </Filter.Footer>
       </Filter>
    );
 };
+export default SearchFilter
