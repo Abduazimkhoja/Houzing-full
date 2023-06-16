d5e05ef170942c04bd5077875429333f1a8f68c9
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-06-16 19:33:28 +0500

    Generic export

--- src/component/Home/index.jsx
+++ src/component/Home/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Intro from "../Intro";
-// import Details from "../Generic/house-details";
 
 const Home = () => {
    return (
       <>
          <Intro />
-         {/* <Details/> */}
       </>
    );
 };
