af9103faeff584362fde59ce1479b867b62b3984
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-06-16 12:10:50 +0500

    slider complete

---
+++ src/component/Carousel/style.js
@@ -0,0 +1,87 @@
+import { styled } from "styled-components";
+
+export const CarouselWrap = styled.section`
+   position: relative;
+   background: "#364d79";
+
+   .slick-dots {
+      align-items: center;
+      gap: 13px;
+
+      &-bottom {
+         bottom: 32px;
+      }
+
+      li {
+         width: 8px;
+         height: 8px;
+         &.slick-active {
+            width: 10px;
+            height: 10px;
+            button {
+               border: 2px solid #ffffff;
+               background: none;
+            }
+         }
+
+         button {
+            background: #fff;
+            opacity: 1;
+            border-radius: 50%;
+            width: 100%;
+            height: 100%;
+         }
+      }
+   }
+`;
+
+
+
+CarouselWrap.Buttons = styled.div`
+   max-width: 1440px;
+   width: 100%;
+   padding: 0 30px;
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%);
+`;
+
+CarouselWrap.Button = styled.button`
+   width: 45px;
+   height: 45px;
+
+   position: absolute;
+   top: 50%;
+   transform: translateY(-50%);
+   ${(props) =>
+      props.name === "left"
+         ? "left: 30px;"
+         : "right: 30px; transform: translateY(-50%) rotate(180deg);"}
+
+   background: #FFFFFF20;
+   border-radius: 50%;
+
+   transition: transform 0.3s ease, background 0.3s ease;
+   &:hover {
+      background: #ffffff40;
+      ${(props) =>
+         props.name === "left"
+            ? "transform: translateY(-50%) scale(1.05);"
+            : "transform: translateY(-50%) rotate(180deg) scale(1.05);"}
+   }
+
+   &:active {
+      background: #ffffff20;
+
+      ${(props) =>
+         props.name === "left"
+            ? "transform: translateY(-50%);"
+            : "transform: translateY(-50%) rotate(180deg);"}
+   }
+
+   i {
+      color: #fff;
+      font-size: 12px;
+   }
+`;
