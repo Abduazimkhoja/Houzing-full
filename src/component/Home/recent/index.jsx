3ee820c3aac6e785e213817d669e8490ee612389
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-06-26 21:51:12 +0500

    recent

---
+++ src/component/Home/recent/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useData } from "../../../hooks/useData";
+// import { useNavigate } from "react-router-dom";
+import { GenericSlider } from "../../Generic";
+import HouseCard from "../../Generic/card/house";
+import { Global } from "../../../root/style";
+import { Wrapper } from "./style";
+
+const Recent = () => {
+   const [data] = useData("/houses/list");
+   // const navigate = useNavigate();
+
+   return (
+      <Wrapper>
+         <Global.H3 margin = "8">Recent Properties for Rent</Global.H3>
+         <Global.H4 ta = "center" margin = "32px">
+            Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
+         </Global.H4>
+         <GenericSlider count={3}>
+            {data.map((object) => (
+               <HouseCard
+                  key={object.id}
+                  data={object}
+                  // onClick={() => navigate(`/properties/?category_id=${value.id}`)}
+               />
+            ))}
+         </GenericSlider>
+      </Wrapper>
+   );
+};
+
+export default Recent;
