c41bf038dd94e7ba89c6a788314dba9f722fe768
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-06-27 22:47:29 +0500

    house card page

---
+++ src/pages/HouseItem/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HouseItem from '../../component/HouseItem'
+
+const HouseItemPage = () => {
+  return (
+    <div><HouseItem/></div>
+  )
+}
+
+export default HouseItemPage
