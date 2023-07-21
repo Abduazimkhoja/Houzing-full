617d8d76a2844cc3c949e21a1e61862f4764952c
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-07-21 11:44:38 +0500

    create add house page

--- src/component/Addhouse/index.jsx
+++ src/component/Addhouse/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Wrapper } from "./style";
+import { Global } from "../../root/style";
 
 const AddHouse = () => {
-   return <div>AddHouse</div>;
+   return (
+      <Wrapper>
+         <Wrapper.Block>
+            <Global.H4 fz="18px" fw="600">
+               Contact information
+            </Global.H4>
+         </Wrapper.Block>
+      </Wrapper>
+   );
 };
 
 export default AddHouse;
---
+++ src/component/Addhouse/style.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.div``;
+
+Wrapper.Block = styled.section`
+   padding: 24px 30px 48px;
+   width: 1180px;
+   height: 301px;
+   flex-shrink: 0;
+   border-radius: 3px;
+   border: 1px solid var(--color-white-smoke, #e6e9ec);
+   background: var(--color-white);
+   box-shadow: 0px 10px 30px 0px rgba(13, 38, 59, 0.05);
+`;
