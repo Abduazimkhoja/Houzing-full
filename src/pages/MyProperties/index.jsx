68ed110c5f6b51bc71324b923da37c2f3844e999
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-07-27 02:06:48 +0500

    create my properties page

---
+++ src/pages/MyProperties/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MyProperties from "../../component/MyProperties";
+
+const MyPropertiesPage = () => {
+   return <MyProperties />;
+};
+
+export default MyPropertiesPage;
