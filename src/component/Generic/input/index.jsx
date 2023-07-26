64a24cb000dc147185e3138c2fa5c1aff930e7e4
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-07-27 02:04:07 +0500

    add new props

--- src/component/Generic/input/index.jsx
+++ src/component/Generic/input/index.jsx
@@ -1,12 +1,12 @@
 import React, { forwardRef } from "react";
 import { InputWrap } from "./style";
 
-export const Input = forwardRef(({name, onChange,onClick, type, placeholder, width, height, icon, bg, defaultValue}, ref) => {
+export const Input = forwardRef(({title, name, onChange,onClick, type, placeholder, width, maxwidth, height, icon, bg, defaultValue, min,max}, ref) => {
 
    return (
-      <InputWrap  ref = {ref} onChange={onChange} type = {type} width={width} height={height} bg={bg}>
-         {icon && <i className={icon}></i>}
-         <InputWrap.Input defaultValue = {defaultValue}  onClick={onClick} name = {name} placeholder={placeholder} type={type} />
+      <InputWrap  ref = {ref} maxwidth={maxwidth} onChange={onChange} type = {type} width={width} height={height} bg={bg}>
+         {icon && <i className={`${icon} "input__icon"`}></i>}
+         <InputWrap.Input min={min} max={max} defaultValue = {defaultValue}  onClick={onClick} name = {name} placeholder={placeholder} type={type}  title={title}/>
       </InputWrap>
    );
 });
