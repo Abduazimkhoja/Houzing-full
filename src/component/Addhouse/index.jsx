94c6eca860e875752f4771e427e98013daa5a030
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-12-23 00:39:33 +0500

    feat: create add house component

--- src/component/Addhouse/index.jsx
+++ src/component/Addhouse/index.jsx
@@ -1,94 +1,137 @@
-import React, { createElement } from "react";
+import { useFormik } from "formik";
+import { createElement, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useRequest from "../../hooks/useRequest";
+import useSearch from "../../hooks/useSearch";
 import { Global } from "../../root/style";
-import { Button, Input } from "../Generic";
+import { houseFormData } from "../../utils/addHouse";
+import { Button } from "../Generic";
 import Contact from "./Contact";
 import Media from "./Media";
-import { addHouseData } from "../../utils/addHouse";
 import { AddHouseStyle } from "./style";
-import { Checkbox } from "antd";
-import { useFormik } from "formik";
-import useRequest from "../../hooks/useRequest";
-import { useNavigate } from "react-router-dom";
+import { RenderInputs } from "./RenderInput";
+import { Spin, message } from "antd";
 
 const AddHouse = () => {
-   const { data, fetchData } = useRequest();
-   const navigate = useNavigate();
+   const { fetchData } = useRequest();
 
-   const formik = useFormik({
-      initialValues: {
-         houseDetails: {},
-         homeAmenitiesDto: {},
-         componentsDto: {},
-         caegory: 1,
-         location: {
-            latitude: 0,
-            longitude: 0,
-         },
+   const initialValues = {
+      address: "",
+      attachments: [],
+      categoryId: 0,
+      city: "",
+      country: "",
+      description: "",
+      houseDetails: {
+         area: "",
+         bath: "",
+         beds: "",
+         garage: "",
+         room: "",
+         yearBuilt: "",
       },
-      onSubmit: async (values) => {
+
+      name: "",
+      price: "",
+      region: "",
+      salePrice: "",
+      zipCode: "",
+
+      status: true,
+      locations: {
+         latitude: 0,
+         longitude: 0,
+      },
+      componentsDto: {},
+      homeAmenitiesDto: {},
+   };
+
+   const navigate = useNavigate();
+   const query = useSearch();
+   const id = query.get("id");
+
+   // formik rendering ---
+   const formik = useFormik({
+      initialValues,
+      enableReinitialize: true,
+
+      onSubmit: (values) => {
          const request = {
-            url: "/houses",
-            method: "POST",
+            url: id ? `/houses/${id}` : "/houses",
+            method: id ? "PUT" : "POST",
             authorize: true,
             body: values,
          };
+         id ? message.success("House updated") : message.success("House added");
 
-         fetchData(request);
+         fetchData(request).then(
+            (res) => res.success && navigate("/myproperties")
+         );
       },
    });
 
-   data?.success && navigate("/myproperties");
+   useEffect(() => {
+      if (id) {
+         fetchData({ url: `/houses/id/${id}`, authorize: true }).then((res) => {
+            formik.setValues(res?.data);
+         });
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
 
-   return (
+   // Block Elements ---
+   const renderingBlockElements = ({ parentObjectName, inputs, component }) => {
+      if (component) {
+         return createElement({ Contact, Media }[component], formik);
+      }
+
+      const attr = {
+         onChange: formik.handleChange,
+         maxwidth: "15%",
+         width: "100%",
+         parentObjectName,
+         inputs,
+         formikValue: formik.values,
+      };
+
+      return (
+         <Global.FlexRowCenter gap="20px">
+            {RenderInputs(attr)}
+         </Global.FlexRowCenter>
+      );
+   };
+
+   // Content Block ---
+   const renderingContentBlock = () => {
+      return houseFormData.map(({ id, title, ...rest }) => {
+         return (
+            <AddHouseStyle.Block key={id}>
+               <Global.H4 fz="18px" fw="600" margin="36px">
+                  {title}
+               </Global.H4>
+               {renderingBlockElements(rest)}
+            </AddHouseStyle.Block>
+         );
+      });
+   };
+
+   // Render Add house
+   return id && !formik?.values?.id ? (
+      <Spin
+         size="large"
+         style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flex: 1,
+         }}
+      />
+   ) : (
       <Global.Wrapper margin="30px 0 100px">
          <Global.Container>
             <form onSubmit={formik.handleSubmit}>
                <Global.H3 margin="26px">Add new property</Global.H3>
-               {addHouseData.map(
-                  ({ id, title, inputs, component, parentObjectName }) => {
-                     return (
-                        <AddHouseStyle.Block key={id}>
-                           <Global.H4 fz="18px" fw="600" margin="36px">
-                              {title}
-                           </Global.H4>
-                           {component ? (
-                              createElement({ Contact, Media }[component], {
-                                 change: formik.handleChange,
-                                 formikValue: formik.values,
-                              })
-                           ) : (
-                              <Global.FlexRowCenter gap="20px">
-                                 {inputs.map((inputAtribute) => {
-                                    return createElement(
-                                       inputAtribute?.type === "checkbox"
-                                          ? Checkbox
-                                          : Input,
-                                       {
-                                          key: inputAtribute?.id,
-                                          ...inputAtribute,
-                                          title: inputAtribute?.placeholder,
-
-                                          checked:
-                                             formik.values[inputAtribute?.name],
-                                          value: formik.values[
-                                             inputAtribute?.name
-                                          ],
-                                          onChange: formik.handleChange,
-
-                                          maxwidth: "15%",
-                                          width: "100%",
-                                       },
-                                       inputAtribute?.type !== "checkbox"
-                                          ? null
-                                          : inputAtribute?.placeholder
-                                    );
-                                 })}
-                              </Global.FlexRowCenter>
-                           )}
-                        </AddHouseStyle.Block>
-                     );
-                  }
-               )}
+               {renderingContentBlock()}
                <Global.Flex jc="right" fxd="row">
                   <Button type="submit" width="280px">
                      Save
