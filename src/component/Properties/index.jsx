a66ea8be0133ef7764a6a2e100922245eb1728e4
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-12-23 00:47:46 +0500

    fix: add title

--- src/component/Properties/index.jsx
+++ src/component/Properties/index.jsx
@@ -6,12 +6,19 @@ import { useData } from "../../hooks/useData";
 
 const Properties = () => {
    const [data] = useData("/houses/list", true);
+   const favoriteList = useData(`/houses/getAll/favouriteList`)[0];
 
    return (
       <Global.Container>
+         <Global.H3 mt="30px" margin="20px">
+            Properties
+         </Global.H3>
+         <Global.H4 margin="60px" ta="center">
+            Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
+         </Global.H4>
          <Wrapper>
             {data?.map((data) => {
-               return <HouseCard key={data?.id} data={data} />;
+               return <HouseCard key={data?.id} data={data} favoriteList={favoriteList}/>;
             })}
          </Wrapper>
       </Global.Container>
