3072e14f7f403cd184a388fc577ea75f802436dc
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-12-24 23:20:47 +0500

    refactor: add error handling and loading attribute

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 
 export const useData = (shiftUrl = "", needSearch = false) => {
    const [data, setData] = useState([]);
+   const [loading, setLoading] = useState(true);
    const { search } = useLocation();
 
    const { REACT_APP_BASE_URL: url } = process.env;
@@ -11,15 +12,25 @@ export const useData = (shiftUrl = "", needSearch = false) => {
    const fullUrl = `${url}${shiftUrl}${searchStatus}`;
 
    const request = async () => {
-      const response = await fetch(fullUrl, {
-         headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-         },
-      });
+      try {
+         const response = await fetch(fullUrl, {
+            headers: {
+               Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+         });
 
-      const responseJson = await response.json();
+         if (!response.ok) {
+            throw new Error(`Ошибка при запросе: ${response.statusText}`);
+         }
 
-      setData(responseJson?.data || []);
+         const responseJson = await response.json();
+
+         setData(responseJson?.data || []);
+      } catch (error) {
+         console.error("Произошла ошибка при запросе:", error.message); // добавлен отладочный вывод
+      } finally {
+         setLoading(false);
+      }
    };
 
    useEffect(() => {
@@ -27,5 +38,5 @@ export const useData = (shiftUrl = "", needSearch = false) => {
       // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [searchStatus]);
 
-   return [data];
+   return [data, loading];
 };
